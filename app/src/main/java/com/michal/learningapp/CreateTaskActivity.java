9d5e40aa3252164340534cc2149ff3d3aca08305
michal <michal.sztaba@op.pl>
2024-07-09 23:27:38 +0200

    Initial commit

---
+++ app/src/main/java/com/michal/learningapp/CreateTaskActivity.java
@@ -0,0 +1,48 @@
+package com.michal.learningapp;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateTaskActivity extends AppCompatActivity {
+
+    DatePickerDialog datePickerDialog;
+    Button dateButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_task);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        
+        initDatePicker();
+        dateButton = (Button) findViewById(R.id.datePickerbtn);
+    }
+
+    private void initDatePicker() {
+        datePickerDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        });
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return day + " " + month + " " + year;
+    }
+}
