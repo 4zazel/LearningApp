4a8e78cd0bd1d1b76ca628b756014ee5d179bce6
michal <michal.sztaba@op.pl>
2024-07-10 22:26:17 +0200

    Added a settings button (need to change icon) (Need to fix task list still)

--- app/src/main/java/com/michal/learningapp/MainActivity.java
+++ app/src/main/java/com/michal/learningapp/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button TaskButton;
+    Button SettingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +28,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
         TaskButton = (Button) findViewById(R.id.Taskbtn);
+        SettingsButton = (Button) findViewById(R.id.settingsbtn);
 
         TaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, TaskActivity.class));
+            }
+        });
+
+        SettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
             }
         });
     }
